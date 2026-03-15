9e1d5c63f69e525f02f9cc91869aac49e0e2ae52
ch-zzy <123283435+ch-zzzy@users.noreply.github.com>
2026-03-15 17:50:30 -0500

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+
+using namespace geode::prelude;
+
+// Removes rounding from PlayerObject::setYVelocity
+class $modify(PlayerObject) {
+	void setYVelocity(double velocity, int type) {
+		this->m_yVelocity = velocity;
+	}
+};
+
+// Patches the rounding from PlayerObject::updateJump
+$on_mod(Loaded) {
+	std::vector<uint8_t> nops(0x26, 0x90);
+	Mod::get()->patch(
+		reinterpret_cast<void*>(base::get() + 0x38C327),
+		nops
+	);
+}
