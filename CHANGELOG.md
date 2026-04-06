15e6dc1413ee13a94e6fbcdb13fea7ac4196f8c4
ch-zzy <123283435+ch-zzzy@users.noreply.github.com>
2026-04-05 19:09:25 -0500

    v1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-# v1.0.1
-- Initial release 
+# v1.0.3
+- Discovered more instances of yvel rounding, patched them as well
+- Added github repo link in mod.json
 # v1.0.2
 - Added "cheat" tag to mod.json
+# v1.0.1
+- Initial release 
--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,21 @@ class $modify(PlayerObject) {
 	}
 };
 
-// Patches the rounding from PlayerObject::updateJump
 #ifdef GEODE_IS_WINDOWS
-$on_mod(Loaded) {
-	std::vector<uint8_t> nops(0x26, 0x90);
+static void ApplyNopPatch(ptrdiff_t offset, size_t size) {
+	std::vector<uint8_t> nops(size, 0x90);
 	Mod::get()->patch(
-		reinterpret_cast<void*>(base::get() + 0x38C327),
+		reinterpret_cast<void*>(base::get() + offset),
 		nops
 	);
+}
+
+$on_mod(Loaded) {
+	// PlayerObject patches
+	ApplyNopPatch(0x38C329, 0x24); // updateJump yvel rounding 
+	ApplyNopPatch(0x213EA2, 0x32); // checkCollisions yvel rounding
+	ApplyNopPatch(0x39323B, 0x40); // collidedWithObjectInternal yvel rounding
+	ApplyNopPatch(0x38DAC7, 0x38); // postCollision yvel rounding
+	ApplyNopPatch(0x39FF18, 0x32); // boostPlayer yvel rounding
 }
 #endif
